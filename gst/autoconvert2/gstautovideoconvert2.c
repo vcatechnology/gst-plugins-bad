eeac37f02c62bee70a9bbce7e26f2a9248fc6ee3
Joel Holdsworth <joel.holdsworth@vcatechnology.com>
2018-07-25 14:27:54 +0000

    autovideoconvert2: Implement factories

--- gst/autoconvert2/gstautovideoconvert2.c
+++ gst/autoconvert2/gstautovideoconvert2.c
@@ -31,10 +31,24 @@
 #include "config.h"
 #endif
 
+#include <string.h>
+
 #include "gstautovideoconvert2.h"
 
 GST_DEBUG_CATEGORY (autovideoconvert2_debug);
 #define GST_CAT_DEFAULT (autovideoconvert2_debug)
+
+static GList *gst_auto_video_convert2_get_factories (GstAutoConvert2 *
+    autoconvert2);
+
+static gboolean element_filter (GstPluginFeature * feature,
+    GstAutoVideoConvert2 * autovideoconvert2);
+static GList *create_factory_list (GstAutoVideoConvert2 * autovideoconvert2);
+static void update_factory_list (GstAutoVideoConvert2 * autovideoconvert2);
+
+static GMutex factories_mutex;
+static guint32 factories_cookie = 0;    /* Cookie from last time when factories was updated */
+static GList *factories = NULL; /* factories we can use for selecting elements */
 
 G_DEFINE_TYPE (GstAutoVideoConvert2, gst_auto_video_convert2,
     GST_TYPE_AUTO_CONVERT2);
@@ -42,11 +56,94 @@ G_DEFINE_TYPE (GstAutoVideoConvert2, gst_auto_video_convert2,
 static void
 gst_auto_video_convert2_class_init (GstAutoVideoConvert2Class * klass)
 {
+  GstAutoConvert2Class *const gstautoconvert2_class =
+      (GstAutoConvert2Class *) klass;
+
   GST_DEBUG_CATEGORY_INIT (autovideoconvert2_debug, "autovideoconvert2", 0,
       "autovideoconvert2 element");
+
+  gstautoconvert2_class->get_factories =
+      GST_DEBUG_FUNCPTR (gst_auto_video_convert2_get_factories);
 }
 
 static void
 gst_auto_video_convert2_init (GstAutoVideoConvert2 * autovideoconvert2)
 {
+}
+
+static GList *
+gst_auto_video_convert2_get_factories (GstAutoConvert2 * autoconvert2)
+{
+  GstAutoVideoConvert2 *autovideoconvert2 =
+      (GstAutoVideoConvert2 *) autoconvert2;
+  update_factory_list (autovideoconvert2);
+  return factories;
+}
+
+static gboolean
+element_filter (GstPluginFeature * feature,
+    GstAutoVideoConvert2 * autovideoconvert2)
+{
+  const gchar *klass;
+
+  /* We only care about element factories */
+  if (G_UNLIKELY (!GST_IS_ELEMENT_FACTORY (feature)))
+    return FALSE;
+
+  klass = gst_element_factory_get_metadata (GST_ELEMENT_FACTORY_CAST (feature),
+      GST_ELEMENT_METADATA_KLASS);
+  /* only select color space converter */
+  if (strstr (klass, "Filter") &&
+      strstr (klass, "Converter") && strstr (klass, "Video")) {
+    GST_DEBUG_OBJECT (autovideoconvert2,
+        "gst_auto_video_convert2_element_filter found %s\n",
+        gst_plugin_feature_get_name (GST_PLUGIN_FEATURE_CAST (feature)));
+    return TRUE;
+  }
+
+  return FALSE;
+}
+
+static GList *
+create_factory_list (GstAutoVideoConvert2 * autovideoconvert2)
+{
+  GList *result = NULL;
+
+  /* get the feature list using the filter */
+  result = gst_registry_feature_filter (gst_registry_get (),
+      (GstPluginFeatureFilter) element_filter, FALSE, autovideoconvert2);
+
+  /* sort on rank and name */
+  result = g_list_sort (result, gst_plugin_feature_rank_compare_func);
+
+  return result;
+}
+
+static void
+update_factory_list (GstAutoVideoConvert2 * autovideoconvert2)
+{
+  /* use a static mutex to protect factories list and factories cookie */
+  g_mutex_lock (&factories_mutex);
+
+  /* test if a factories list already exist or not */
+  if (!factories) {
+    /* no factories list create it */
+    factories_cookie =
+        gst_registry_get_feature_list_cookie (gst_registry_get ());
+    factories = create_factory_list (autovideoconvert2);
+  } else {
+    /* a factories list exist but is it up to date? */
+    if (factories_cookie !=
+        gst_registry_get_feature_list_cookie (gst_registry_get ())) {
+      /* we need to update the factories list */
+      /* first free the old one */
+      gst_plugin_feature_list_free (factories);
+      /* then create an updated one */
+      factories_cookie =
+          gst_registry_get_feature_list_cookie (gst_registry_get ());
+      factories = create_factory_list (autovideoconvert2);
+    }
+  }
+
+  g_mutex_unlock (&factories_mutex);
 }
