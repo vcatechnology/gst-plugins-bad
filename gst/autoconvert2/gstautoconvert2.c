bfc6346c754c1de0c7e866feb6f9f5e9e7041d0a
Joel Holdsworth <joel.holdsworth@vcatechnology.com>
2018-07-25 12:30:04 +0000

    autoconvert2: Added sink and source request pads

--- gst/autoconvert2/gstautoconvert2.c
+++ gst/autoconvert2/gstautoconvert2.c
@@ -51,6 +51,16 @@ GST_DEBUG_CATEGORY (autoconvert2_debug);
 #define GST_AUTO_CONVERT2_UNLOCK(autoconvert2) \
   (g_mutex_unlock (GST_AUTO_CONVERT2_GET_LOCK (autoconvert2)))
 
+static GstStaticPadTemplate sinktemplate = GST_STATIC_PAD_TEMPLATE ("sink_%u",
+    GST_PAD_SINK,
+    GST_PAD_REQUEST,
+    GST_STATIC_CAPS_ANY);
+
+static GstStaticPadTemplate srctemplate = GST_STATIC_PAD_TEMPLATE ("src_%u",
+    GST_PAD_SRC,
+    GST_PAD_REQUEST,
+    GST_STATIC_CAPS_ANY);
+
 struct _GstAutoConvert2Priv
 {
   /* Lock to prevent caps pipeline structure changes during changes to pads. */
@@ -58,6 +68,10 @@ struct _GstAutoConvert2Priv
 };
 
 static void gst_auto_convert2_finalize (GObject * object);
+
+static GstPad *gst_auto_convert2_request_new_pad (GstElement * element,
+    GstPadTemplate * templ, const gchar * name, const GstCaps * caps);
+static void gst_auto_convert2_release_pad (GstElement * element, GstPad * pad);
 
 #define gst_auto_convert2_parent_class parent_class
 G_DEFINE_TYPE (GstAutoConvert2, gst_auto_convert2, GST_TYPE_BIN);
@@ -76,6 +90,14 @@ gst_auto_convert2_class_init (GstAutoConvert2Class * klass)
       "Creates a graph of transform elements based on the caps",
       "Joel Holdsworth <joel.holdsworth@vcatechnology.com>");
 
+  gst_element_class_add_static_pad_template (gstelement_class, &srctemplate);
+  gst_element_class_add_static_pad_template (gstelement_class, &sinktemplate);
+
+  gstelement_class->request_new_pad =
+      GST_DEBUG_FUNCPTR (gst_auto_convert2_request_new_pad);
+  gstelement_class->release_pad =
+      GST_DEBUG_FUNCPTR (gst_auto_convert2_release_pad);
+
   gobject_class->finalize = GST_DEBUG_FUNCPTR (gst_auto_convert2_finalize);
 }
 
@@ -93,4 +115,34 @@ gst_auto_convert2_finalize (GObject * object)
 
   g_mutex_clear (&autoconvert2->priv->lock);
   g_free (autoconvert2->priv);
+}
+
+static GstPad *
+gst_auto_convert2_request_new_pad (GstElement * element,
+    GstPadTemplate * templ, const gchar * name, const GstCaps * caps)
+{
+  GstAutoConvert2 *const autoconvert2 = GST_AUTO_CONVERT2 (element);
+  GstPad *const pad = gst_ghost_pad_new_no_target_from_template (NULL, templ);
+
+  GST_AUTO_CONVERT2_LOCK (autoconvert2);
+
+  if (gst_element_add_pad (element, pad)) {
+    GST_AUTO_CONVERT2_UNLOCK (autoconvert2);
+    return pad;
+  }
+
+  GST_DEBUG_OBJECT (autoconvert2, "could not add pad");
+  gst_object_unref (pad);
+  GST_AUTO_CONVERT2_UNLOCK (autoconvert2);
+  return NULL;
+}
+
+static void
+gst_auto_convert2_release_pad (GstElement * element, GstPad * pad)
+{
+  GstAutoConvert2 *const autoconvert2 = GST_AUTO_CONVERT2 (element);
+
+  GST_AUTO_CONVERT2_LOCK (autoconvert2);
+  gst_element_remove_pad (element, pad);
+  GST_AUTO_CONVERT2_UNLOCK (autoconvert2);
 }
